999af88d5b4bca2c656c371b3c8156c12a68c5fb
SirGoatsalot <fostersullivan12@gmail.com>
2023-01-30 15:53:11 -0500

    Add missed files

--- client/public/main.js
+++ client/public/main.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from '../components/App.jsx';
-// import Routing from '../components/Routing.jsx';
-
-// uncomment so that webpack can bundle styles
-// import styles from './stylesheets/styles.scss';
-
-render(
-//   <BrowserRouter>
-//     {/* <Routing /> */}
-//   </BrowserRouter>, document.getElementById('root')
-<App /> , document.getElementById('root')
-);
-
 // import React from 'react';
-// import { createRoot } from 'react-dom/client';
+// import { render } from 'react-dom';
+// import { BrowserRouter } from 'react-router-dom';
 // import App from '../components/App.jsx';
-// const container = document.getElementById('root');
-// const root = createRoot(container);
+// // import Routing from '../components/Routing.jsx';
 
+// // uncomment so that webpack can bundle styles
+// // import styles from './stylesheets/styles.scss';
 
-
-// root.render(
-//   <App />
+// render(
+// //   <BrowserRouter>
+// //     {/* <Routing /> */}
+// //   </BrowserRouter>, document.getElementById('root')
+// <App /> , document.getElementById('root')
 // );
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '../components/App.jsx';
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+
+
+root.render(
+  <App />
+);
