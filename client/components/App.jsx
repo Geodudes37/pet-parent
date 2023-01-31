593b6fac9da46dd3ba301cdc2e216af0eb042af3
Garima Bhatia <marigolddancestudio@gmail.com>
2023-01-31 00:12:40 -0500

    this commit will add basic routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,7 @@ import Login from '../components/Login.jsx';
 import CreateUser from '../components/CreateUser.jsx';
 import Dashboard from '../components/Dashboard.jsx'
 import '../public/stylesheets/styles.scss'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 export const SCREEN_MAP = {
     login: 'login',
@@ -14,17 +15,32 @@ const App = () => {
 
   const [currentScreen, setCurrentScreen] = useState(SCREEN_MAP.login);
   return(
-    <div className='app-page'>
-        {currentScreen === SCREEN_MAP.createUser && (
-            <CreateUser setCurrentScreen={setCurrentScreen}/>
-        )}
-        {currentScreen === SCREEN_MAP.login && (
-          <Login setCurrentScreen={setCurrentScreen}/>
-        )}
-        {currentScreen === SCREEN_MAP.dashboard && (
-          <Dashboard setCurrentScreen={setCurrentScreen}/>
-        )}
-    </div>
+    <Router>
+      <div className='app-page'>
+
+        {/* <Link to="/login">Login</Link>
+        <Link to="/create-user">Create User</Link>
+        <Link to="/">Dashboard</Link> */}
+
+        <Routes>
+          <Route exact path='/' element={<Dashboard/>}/>
+          <Route exact path='/login' element={<Login/>}/>
+          <Route exact path='/create-user' element={<CreateUser/>}/>
+
+
+          {/* {currentScreen === SCREEN_MAP.createUser && (
+              <CreateUser setCurrentScreen={setCurrentScreen}/>
+          )}
+          {currentScreen === SCREEN_MAP.login && (
+            <Login setCurrentScreen={setCurrentScreen}/>
+          )}
+          {currentScreen === SCREEN_MAP.dashboard && (
+            <Dashboard setCurrentScreen={setCurrentScreen}/>
+          )} */}
+          
+        </Routes>
+      </div>
+    </Router>
   ) 
 }
 
--- client/components/CreateUser.jsx
+++ client/components/CreateUser.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { SCREEN_MAP } from './App.jsx';
+import { Link } from 'react-router-dom';
+
 
 const CreateUser = (props) => {
-  return (<div className='create-user'>
+  return (<div className='create-user container'>
     Create User!
-    <button onClick={() => {props.setCurrentScreen(SCREEN_MAP.dashboard)}}>Go to dashboard!</button>
+    {/* <button onClick={() => {props.setCurrentScreen(SCREEN_MAP.dashboard)}}>Go to dashboard!</button> */}
+    <Link to="/">Go to dashboard!</Link>
   </div>);
 }
 
