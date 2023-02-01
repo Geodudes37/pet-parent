0a28c3461348568bf16796104b452776f50371cd
Garima Bhatia <marigolddancestudio@gmail.com>
2023-02-01 10:18:33 -0500

    will commit changes in the login file

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,76 +1,77 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-
 const Login = (props) => {
-
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const loginRequest = async(event) =>{
+  const loginRequest = async (event) => {
     //this will prevent the default behaviour of the onSubmit page which is refreshing the page
-    event.preventDefault()
+    event.preventDefault();
     const requestBody = {
       username,
       password,
     };
-    try{
-      const response = await fetch('/login', {
-        method: 'POST',
-        headers: {'Content-Type':'application/json'},
+    try {
+      const response = await fetch("/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(requestBody),
       });
-      const data = await response.json()
-      if(data){
+      const data = await response.json();
+      if (data) {
         navigate("/preferences");
       }
-    }catch(err){
-      console.log(err)
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
   return (
     <>
-    <header className='logo'>Welcome to Pet-Parent</header>
-    <form className="login"
-    onSubmit={loginRequest}
-    >
-    <div className='container'>
-    <div className="form-title" id="form-title">Login</div>
-    <label>
-      Username
-      <input 
-      value={username}
-      onChange={(event)=>{
-        setUsername(event.target.value)
-      }}
-      type="text" 
-      />
-    </label>
-    <label>
-      Password
-      <input 
-      value={password}
-      onChange={(event)=>{
-        setPassword(event.target.value)
-      }}
-      type="password" 
-      />
-    </label>
-    <button disabled={!username || !password} >Log In</button>
-    <div className='divider' />
-    <button className='create-user'
-    onClick={()=>{
-      navigate("/create-user")
-    }}
-    >Click here to Sign Up</button>
-    {/* <Link to="/dashboard">Dashboard should be protected</Link>
+      <header className="logo">Welcome to Pet-Parent</header>
+      <form className="login" onSubmit={loginRequest}>
+        <div className="container">
+          <div className="form-title" id="form-title">
+            Login
+          </div>
+          <label>
+            Username
+            <input
+              value={username}
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+              type="text"
+            />
+          </label>
+          <label>
+            Password
+            <input
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+              type="password"
+            />
+          </label>
+          <button disabled={!username || !password}>Log In</button>
+          <div className="divider" />
+          <button
+            className="create-user"
+            onClick={() => {
+              navigate("/create-user");
+            }}
+          >
+            Click here to Sign Up
+          </button>
+          {/* <Link to="/dashboard">Dashboard should be protected</Link>
     <Link to="/preferences">Preferences should be protected</Link> */}
-    </div>
-  </form>
-  </>
+        </div>
+      </form>
+    </>
   );
-}
+};
 
 export default Login;
